5e4c7f0a4ad00a52513553db41c5a06646118739
charlie125 <93170343+charlie125@users.noreply.github.com>
2024-06-16 19:05:52 +0800

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -1,8 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-#word = input( '請輸入中文字:' )
-
-
 def read (word):
     url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
     
