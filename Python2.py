606dae9f8c17c99bd194b748948773dab89b24d9
charlie125 <93170343+charlie125@users.noreply.github.com>
2024-06-24 16:14:38 +0800

    Update Python2.py

--- Python2.py
+++ Python2.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-def read (word):
-    url= f"http://dict.cn/big5/{word}"
-    html = requests.get( url )
-    soup = BeautifulSoup(html.text,'lxml')
+def read(word):
+    url = f"http://dict.cn/big5/{word}"
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'lxml')
     data = soup.find('div', class_='layout sort')
     try:
         english = data.find_all('li')
         en = [e.text for e in english]
-        sen = ' \n '.join(en)
-        return( sen )
-    except:
-        return("Error404")
+        sen = '\n'.join(en)  # 使用 "\n" 換行符號來連接翻譯結果
+        return sen
+    except AttributeError:
+        return "Error404"
