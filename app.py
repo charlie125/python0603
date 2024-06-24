21f7d0061063588bcca746a7040e1659cae80160
charlie125 <93170343+charlie125@users.noreply.github.com>
2024-06-24 15:32:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def callback():
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=event.message.text)
+            TextSendMessage(text=ph.read(event.message.text))
         )
 
     return 'OK'
